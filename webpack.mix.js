d1742c9d24a760b8b18a36ca4a5d22e5b1fab550
Norbert Skwierczynski <norbert.fe.dev@gmail.com>
2024-05-11 21:12:39 +0100

    init commit Laravel php & tailwind

--- webpack.mix.js
+++ webpack.mix.js
@@ -4,4 +4,3 @@ mix.js('resources/js/app.js', 'public/js')
    .postCss('resources/css/app.css', 'public/css', [
         require('tailwindcss'),
     ]);
-// Path: resources/views/layouts/app.blade.php
